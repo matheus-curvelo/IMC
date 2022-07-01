83c7c21d84fadb6c16b352e169fa5770070c7db4
Matheus Curvelo <matheuscurvelo@gmail.com>
2022-06-30 22:20:41 -0300

    Inicio de projeto

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Component} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  this.state = { 
+
+   }
+
+  }
+  render() { 
+    return ( 
+      <View>
+        
+      </View>
+     );
+  }
+}
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+export default App;
